9f2b941efa1163722905232a2a6d8a23038aad4f
00lansefeixiang <848147319@qq.com>
2017-07-02 20:55:34 +0900

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,4 +1,18 @@
 
 public class Card {
-
+//	 private String color;
+//	 private String number;
+//
+//	 public Card(String color, String number) {
+//	     this.color = color;
+//	    this.number = number;
+//	 }
+//
+//	 public String getColor() {
+//		 return color;
+//	 }
+//
+//	 public String getNumber() {
+//	     return number;
+//	 }
 }
